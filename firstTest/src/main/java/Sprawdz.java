e13da32bffc9878ae16ad19548fbee1553481119
Lukas <lukaszsolarz@icloud.com>
2019-12-28 22:11:59 +0100

    Dodaje After class

--- firstTest/src/main/java/Sprawdz.java
+++ firstTest/src/main/java/Sprawdz.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.TestNG;
 import org.openqa.selenium.WebElement;
@@ -9,10 +10,11 @@ import org.openqa.selenium.WebDriver;
 import static org.testng.Assert.assertTrue;
 
 public class Sprawdz {
+    WebDriver driver = new ChromeDriver();
 
     @Test
     public void Testowa() {
-        WebDriver driver = new ChromeDriver();
+
         driver.get("https://google.com");
         String ActualPage = driver.getTitle();
         String ExpectedPage = "Google";
@@ -20,5 +22,13 @@ public class Sprawdz {
         Assert.assertEquals(ActualPage, ExpectedPage);    //1 sposób
         assertTrue(driver.getTitle().equals(ExpectedPage), "Strona jest niepoprawna ");  // 2 sposób z komentarzem
 
+
+
     }
+
+    @AfterClass
+    public void Zniszcz(){
+        driver.quit();
+    }
+
 }
